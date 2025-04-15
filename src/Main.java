2f67a6212dd9fa824fbf2445539573d713bdef40
Ashly968 <ashlyangon1@gmail.com>
2025-04-15 15:55:16 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,25 @@ public class Main {
     public static void main(String[] args) {
         Tile mudTile = new TileType.Mud();
         Tile grassTile = new TileType.Grass();
+        Tile meadowTile = new TileType.Meadow();
+        Tile rubbleTile = new TileType.Rubble();
+        Tile clayTile = new TileType.Clay();
+        Tile stone_dirtTile = new TileType.Stone_Dirt();
+        Tile stone_pathTile = new TileType.Stone_Path();
         Tile roadTile = new TileType.Road();
         Tile waterTile = new TileType.Water();
         Tile dirtTile = new TileType.Dirt();
 
         Value_Implement valueEntity_1 = new Value_Implement(mudTile);
         Value_Implement valueEntity_2 = new Value_Implement(grassTile);
-        Value_Implement valueEntity_3 = new Value_Implement(roadTile);
-        Value_Implement valueEntity_4 = new Value_Implement(waterTile);
-        Value_Implement valueEntity_5 = new Value_Implement(dirtTile);
+        Value_Implement valueEntity_3 = new Value_Implement(MeadowTile);
+        Value_Implement valueEntity_4 = new Value_Implement(rubbleTile);
+        Value_Implement valueEntity_5 = new Value_Implement(clayTile);
+        Value_Implement valueEntity_6 = new Value_Implement(stone_dirtTile);
+        Value_Implement valueEntity_7 = new Value_Implement(stone_pathTile);
+        Value_Implement valueEntity_8 = new Value_Implement(roadTile);
+        Value_Implement valueEntity_9 = new Value_Implement(waterTile);
+        Value_Implement valueEntity_10 = new Value_Implement(dirtTile);
 
         Character character = new Character("Hero", new Position(0, 0), new ResourcePoints(100, 50));
 
